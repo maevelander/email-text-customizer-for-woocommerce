d50088c49957ca9d54441ce3885b016c003f3ec5
imomer <omerblink@gmail.com>
2017-05-30 15:32:04 +0500

    Fixed the issue with broken email panel

--- email-text-customizer-for-woocommerce.php
+++ email-text-customizer-for-woocommerce.php
@@ -99,17 +99,17 @@ class WETC {
 		require ('assets/class-wc-email-customer-reset-password.php');
 		require ('assets/class-wc-email-customer-new-account.php');
 
-		$email_classes['WC_Email_New_Order']                 = include ('assets/class-wc-email-new-order.php');
-		$email_classes['WC_Email_Cancelled_Order']           = include ('assets/class-wc-email-cancelled-order.php');
-		$email_classes['WC_Email_Failed_Order']              = include ('assets/class-wc-email-failed-order.php');
-		$email_classes['WC_Email_Customer_On_Hold_Order']    = include ('assets/class-wc-email-customer-on-hold-order.php');
-		$email_classes['WC_Email_Customer_Processing_Order'] = include ('assets/class-wc-email-customer-processing-order.php');
-		$email_classes['WC_Email_Customer_Completed_Order']  = include ('assets/class-wc-email-customer-completed-order.php');
-		$email_classes['WC_Email_Customer_Refunded_Order']   = include ('assets/class-wc-email-customer-refunded-order.php');
-		$email_classes['WC_Email_Customer_Invoice']          = include ('assets/class-wc-email-customer-invoice.php');
-		$email_classes['WC_Email_Customer_Note']             = include ('assets/class-wc-email-customer-note.php');
-		$email_classes['WC_Email_Customer_Reset_Password']   = include ('assets/class-wc-email-customer-reset-password.php');
-		$email_classes['WC_Email_Customer_New_Account']      = include ('assets/class-wc-email-customer-new-account.php');
+		$email_classes['WC_Email_New_Order']                 = new WETCNewOrder();
+		$email_classes['WC_Email_Cancelled_Order']           = new WETCCancelledOrder();
+		$email_classes['WC_Email_Failed_Order']              = new WETCFailedOrder();
+		$email_classes['WC_Email_Customer_On_Hold_Order']    = new WETCOrderOnHold();
+		$email_classes['WC_Email_Customer_Processing_Order'] = new WETCProcessingOrder();
+		$email_classes['WC_Email_Customer_Completed_Order']  = new WETCCompletedOrder();
+		$email_classes['WC_Email_Customer_Refunded_Order']   = new WETCRefundedOrder();
+		$email_classes['WC_Email_Customer_Invoice']          = new WETCInvoice();
+		$email_classes['WC_Email_Customer_Note']             = new WETCNote();
+		$email_classes['WC_Email_Customer_Reset_Password']   = new WETCResetPassword();
+		$email_classes['WC_Email_Customer_New_Account']      = new WETCNewAccount();
 
 		return $email_classes;
 
